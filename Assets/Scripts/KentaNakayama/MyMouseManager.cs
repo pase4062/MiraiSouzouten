b3f4217a838cbe871107bf433748adaef20c1e5e
DESKTOP-LKA5NOS\knt14 <kenta.n.75061@gmail.com>
2020-11-06 14:48:50 +0900

    残機表示および「damage」のタグが付いてるオブジェクトをクリックすると残機が減る。リトライとゲームオーバーは未実装。

---
+++ Assets/Scripts/KentaNakayama/MyMouseManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyMouseManager : MonoBehaviour
+{
+    GameObject clickedGameObject;
+    MyObjController objcontroller;
+
+    public GameObject[] playerIcons;
+
+    public int destroyCount = 0;
+
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            clickedGameObject = null;
+            objcontroller = null;
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit2d = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction);
+
+            if (hit2d)
+            {
+                clickedGameObject = hit2d.transform.gameObject;
+
+                if (clickedGameObject.gameObject.CompareTag("damage"))
+                {
+                    destroyCount += 1;
+                    UpdatePlayerIcons();
+                }
+
+                
+
+                // クリックしたオブジェクトのイベント確認
+                //objcontroller = clickedGameObject.GetComponent<MyObjController>();
+                //objcontroller.GetActive();
+            }
+
+            Debug.Log(clickedGameObject);
+        }
+    }
+
+    void UpdatePlayerIcons()
+    {
+        // for文（繰り返し文）・・・まずは基本形を覚えましょう！
+        for (int i = 0; i < playerIcons.Length; i++)
+        {
+            if (destroyCount <= i)
+            {
+                playerIcons[i].SetActive(true);
+            }
+            else
+            {
+                playerIcons[i].SetActive(false);
+            }
+        }
+    }
+
+
+}
