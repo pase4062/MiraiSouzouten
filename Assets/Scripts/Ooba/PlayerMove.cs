8f0eb1dda7f503a9b8a6e7f449d2a7c0f7c69fef
DESKTOP-TD8FU4F\torom <toromomizi@gmail.com>
2020-11-13 06:09:10 +0900

    プレイヤー待機/歩行モーション追加

---
+++ Assets/Scripts/Ooba/PlayerMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            anim.SetTrigger("Wait to Move");
+        }
+        else
+        {
+            anim.SetTrigger("Move to Wait");
+        }
+        
+    }
+}
