34c349174bd1c6acaaddb41e6302ea7ba8566eea
パセ <yota11271998@gmail.com>
2020-11-19 11:07:31 +0900

    フェードイン、フェードアウトを追加

---
+++ Assets/Scripts/Yanagida/Title/TitleController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(Input.GetKeyDown(KeyCode.A))
+        //{
+        //    FadeManager.Instance.LoadScene("PrototypeStage1", 2.0f);
+        //}
+    }
+
+    public void PlayGame()
+    {
+        FadeManager.Instance.LoadScene("PrototypeStage1", 2.0f);
+    }
+}
